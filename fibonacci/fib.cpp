0999a8afc8403d996cf84fc362e3f48775322d27
BatiFerren <powerferren@gmail.com>
2021-02-18 09:39:19 +0200

    Buffering of calculation for boosting the speed of calculation

--- fibonacci/fib.cpp
+++ fibonacci/fib.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 
-int fibonacci(int n)
-{
+static int32_t buff[100] = {0};
 
-    return n <= 1 ? n : fibonacci(n-1) + fibonacci(n-2);
+int32_t fibonacci(int32_t n)
+{
+    if (n <= 1) return n;
+    if (buff[n] == 0) buff[n] = fibonacci(n-1) + fibonacci(n-2);
+    return buff[n];
 }
 
 int main (int argc, char *argv[])
 {
-    int num = 0;
+    int32_t num = 0;
     std::cout << "Enter number of element of Fibonacci number" << std::endl;
     std::cin >> num;
     std::cout << num << " element of Fibonacci number equals " << fibonacci(num) << std::endl;
