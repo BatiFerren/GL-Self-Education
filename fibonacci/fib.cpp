e011c6eda2163480e5e931e8320799a394e88bcf
BatiFerren <powerferren@gmail.com>
2021-02-18 11:06:03 +0200

    Created function that calculate sum of first n element of Fibonacci number

--- fibonacci/fib.cpp
+++ fibonacci/fib.cpp
@@ -4,9 +4,21 @@ static int32_t buff[100] = {0};
 
 int32_t fibonacci(int32_t n)
 {
-    if (n <= 1) return n;
-    if (buff[n] == 0) buff[n] = fibonacci(n-1) + fibonacci(n-2);
+    if (n <= 1) {buff[0] = 0; buff[1] = 1;}
+    else {buff[n] = fibonacci(n-1) + fibonacci(n-2);}
     return buff[n];
+}
+
+int32_t sum_of_fibonacci(int32_t counter)
+{
+    int32_t sum = 0;
+    for (int32_t i = 0; i <= counter; i++)
+	{
+	    sum += buff[i];
+	}
+
+    return sum;
+
 }
 
 int main (int argc, char *argv[])
@@ -14,7 +26,8 @@ int main (int argc, char *argv[])
     int32_t num = 0;
     std::cout << "Enter number of element of Fibonacci number" << std::endl;
     std::cin >> num;
-    std::cout << num << " element of Fibonacci number equals " << fibonacci(num) << std::endl;
+    fibonacci(num);
+    std::cout << "Sum of " << num << " elements of Fibonacci number equals " << sum_of_fibonacci(num) << std::endl;
 
     return 0;
 }
