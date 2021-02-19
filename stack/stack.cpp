90db408a0204a7f613d5495be2c604e2e612e338
BatiFerren <powerferren@gmail.com>
2021-02-19 12:57:34 +0200

    Created Makefile and help

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -36,13 +36,12 @@ public:
 
     void push(uint32_t element)
     {
-	if (top <= size_stack) {array[top++] = element;}
-	else {std::cout << "You reached the end of the stack. Resize your stack" << std::endl;}
+	if (top < size_stack) {array[top++] = element;}
+	else {std::cout << "You reached the end of the stack. Increase your stack" << std::endl;}
     }
 
     uint32_t pop()
     {
-	//uint32_t index_of_top_element = top - 1;
 	return array[--top];
     }
 
@@ -60,16 +59,17 @@ public:
     {
 	uint32_t *buff;
 	buff = new uint32_t[top];
-	for (uint32_t i = 0; i <= top; i++)
+	for (uint32_t i = 0; i < top; i++)
 	{
 	    buff[i] = array[i];
 	}
 	size_stack = new_size_stack;
 	delete[] array;
 	array = new uint32_t[size_stack];
-	for (uint32_t i = 0; i <= top; i++)
+	top = 0;
+	for (uint32_t i = 0; i < size_stack; i++)
 	{
-	    array[i] = buff[i];
+	    array[top++] = buff[i];
 	}
 	delete[] buff;
     }
@@ -83,7 +83,7 @@ int main (int argc, char *argv[])
     {
 	if (arg == "--help" || arg == "-h")
 	{
-	    std::cout << "This programm create stack based on array." << std::endl;
+	    std::cout << "This programm create stack based on array.\n To create stack use: new_stack.create(n), where new_stack - the name of your stack, n - size of your stack\n Before creation stack it must be declared MyStack new_stack\n To add element to your stack do new_stack.push(element)\n To get top element from stack do new_stack.pop(). Function pop() returns value of the top element and erases it from the stack.\n To get amount of elements in stack do new_stack.capacity(). This function returns amount of elements in stack.\n To get maximum amount of elements in stack use function new_stack.size() that returns value of maximum amount of elements which contains the stack.\n For resizing stack use new_stack.resize(new_size).\n Function new_stack.print_stack prints all elements of the stack.\n" << std::endl;
 	    return 0;
 	}
     }
@@ -98,7 +98,7 @@ int main (int argc, char *argv[])
     std::cout << "Cap = " << cap << std::endl;
     uint32_t siz = mystack.size();
     std::cout << "Size = " << siz << std::endl;
-    mystack.resize(15);
+    mystack.resize(2);
     mystack.push(1);
     uint32_t new_cap = mystack.capacity();
     std::cout << "New_Cap = " << new_cap << std::endl;
@@ -106,11 +106,6 @@ int main (int argc, char *argv[])
     std::cout << "New_Size = " << new_siz << std::endl;
     mystack.print_stack();
     std::cout << "Top element = " << mystack.pop() << std::endl;
-    uint32_t new_cap2 = mystack.capacity();
-    std::cout << "New_Cap2 = " << new_cap2 << std::endl;
-    uint32_t new_siz2 = mystack.size();
-    std::cout << "New_Size2 = " << new_siz2 << std::endl;
-    mystack.print_stack();
 
     return 0;
 }
